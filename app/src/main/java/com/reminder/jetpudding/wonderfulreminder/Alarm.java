ec3896a2d83ffe9ad024e604e7eda926b2dcdbf4
donburako <g13916kh@gm.tsuda.ac.jp>
2016-07-01 21:03:32 +0900

    Alarm.javaとTask.javaを追加しました。

---
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/Alarm.java
@@ -0,0 +1,24 @@
+package com.reminder.jetpudding.wonderfulreminder;
+import android.media.MediaPlayer;
+import java.util.Date;
+/**
+ * Created by mbpusr on 2016/07/01.
+ */
+public class Alarm {
+    private Date endtime;
+    private MediaPlayer mp;
+
+    public void ring(){
+        mp = MediaPlayer.create(this, R.raw.test);
+        // resのrawディレクトリにtest.mp3を置いた場合(今はまだファイルもフォルダもないのでエラー状態)
+        mp.start();
+    }
+    public boolean matchtime(Date currenttime){
+        if(endtime.equals(currenttime.TaskManager)){
+            //TaskManagerを入れてくれればエラーは消えるはず！
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/Task.java
@@ -0,0 +1,56 @@
+package com.reminder.jetpudding.wonderfulreminder;
+import java.util.Date;
+/**
+ * Created by mbpusr on 2016/07/01.
+ */
+public class Task {
+    private int num;
+
+    private String taskname;
+
+    private Date endtime;
+
+    private String detail;
+
+    public Task(int num,String taskname,Date endtime,String detail) {
+        this.num=num;
+        this.taskname=taskname;
+        this.endtime = endtime;
+        this.detail=detail;
+
+    }
+
+    public int getNumber(){
+        return num;
+    }
+
+    public void setNumber(int num) {
+        this.num = num;
+    }
+
+    public String getTaskName(){
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+    public Date getEndTime(){
+        return endtime;
+    }
+
+    public void setEndTime(Date endtime) {
+        this.endtime=endtime;
+    }
+
+    public String getDetail(){
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+}
+
