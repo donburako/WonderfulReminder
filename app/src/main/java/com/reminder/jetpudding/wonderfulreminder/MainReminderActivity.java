a071f3a6e9aed5b36fcfb71d16febf64ee36dfc2
donburako <g13916kh@gm.tsuda.ac.jp>
2016-07-01 20:29:03 +0900

    これを発展させていきましょう

---
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/MainReminderActivity.java
@@ -0,0 +1,13 @@
+package com.reminder.jetpudding.wonderfulreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_reminder);
+    }
+}
