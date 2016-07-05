3321104f3e0f1885a036b2abcf40315d7734f8d6
YukariFujino <mokayumi55>
2016-07-05 13:13:26 +0900

    一部のxmlファイルに対して、警告文に対応。 (他の警告文も原因が分かったら対応)

--- app/src/main/java/com/reminder/jetpudding/wonderfulreminder/MainReminderActivity.java
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/MainReminderActivity.java
@@ -2,12 +2,17 @@ package com.reminder.jetpudding.wonderfulreminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.*;
 
 public class MainReminderActivity extends AppCompatActivity {
 
+    Intent intent=new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_reminder);
+    }
+    public void onClickDecide(){
+        //Main画面へ遷移するように設定
     }
 }
