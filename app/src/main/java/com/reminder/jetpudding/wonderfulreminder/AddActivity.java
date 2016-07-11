09362214b74138c8169e550a232e4966ef373c61
donburako <g13916kh@gm.tsuda.ac.jp>
2016-07-11 17:57:11 +0900

    AddActivity入力値をtaskに入れる。 日付に関しては年月日まで

--- app/src/main/java/com/reminder/jetpudding/wonderfulreminder/AddActivity.java
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/AddActivity.java
@@ -14,13 +14,7 @@ import static com.reminder.jetpudding.wonderfulreminder.AddTask.*;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText TaskName=(EditText)findViewById(R.id.taskname);
-    EditText Detail=(EditText)findViewById(R.id.detail);
-    DatePicker EndTime=(DatePicker)findViewById(R.id.datePicker1);
-    String taskName=TaskName.getText().toString();
-    long loendTime=EndTime.getYear()+EndTime.getMonth()+EndTime.getDayOfMonth();//まだ年と月と日しかとれていない
-    Date enddate = new Date(loendTime);
-    String detail=Detail.getText().toString();
+
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +23,15 @@ public class AddActivity extends AppCompatActivity {
 
 
     public void toOK(View view){
+        EditText TaskName=(EditText)findViewById(R.id.taskname);
+        EditText Detail=(EditText)findViewById(R.id.detail);
+        DatePicker EndTime=(DatePicker)findViewById(R.id.datePicker1);
+        String taskName=TaskName.getText().toString();
+        long loendTime=EndTime.getYear()+EndTime.getMonth()+EndTime.getDayOfMonth();//まだ年と月と日しかとれていない
+        Date enddate = new Date(loendTime);
+        String detail=Detail.getText().toString();
        Task task= new Task(1,taskName,enddate,detail);
-        //AddTask addtask=new Anew AddTask.execute(task);
+        //AddTask addtask=new Anew AddTask.execute(task);*/
        finish();
     }
 }
