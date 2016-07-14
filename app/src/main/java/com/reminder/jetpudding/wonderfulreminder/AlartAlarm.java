255f2a901c4c73e30cbd6cad5dc14a1d1e9267fa
YukariFujino <mokayumi55>
2016-07-14 19:45:57 +0900

    Alarm対応

--- app/src/main/java/com/reminder/jetpudding/wonderfulreminder/AlartAlarm.java
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/AlartAlarm.java
@@ -7,8 +7,12 @@ package com.reminder.jetpudding.wonderfulreminder;
 import android.app.*;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import java.util.*;
+import android.media.*;
 
 public class AlartAlarm extends Activity{
+    private MediaPlayer mp;
+    Alarm alarm=new Alarm();
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -16,16 +20,37 @@ public class AlartAlarm extends Activity{
         setContentView(R.layout.activity_main_reminder);
 
         AlertDialog.Builder dialogBuilder=new AlertDialog.Builder(this);
+        AlertDialog Dialog=dialogBuilder.create();
+        Dialog.show();
         dialogBuilder.setTitle("リマインダー");
         dialogBuilder.setMessage("時間になりました");
+        onStart();
         dialogBuilder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
             public void onClick(DialogInterface dialog,int which){
+                onDestroy();
                 dialog.cancel();
             }
         });
 
         dialogBuilder.setCancelable(true);
-        AlertDialog Dialog=dialogBuilder.create();
-        Dialog.show();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(mp==null){
+            mp=MediaPlayer.create(this,R.raw.test);
+        }
+        mp.start();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        stopAndRelease();
+    }
+    private void stopAndRelease(){
+        if(mp!=null){
+            mp.stop();
+            mp.release();
+        }
     }
 }
