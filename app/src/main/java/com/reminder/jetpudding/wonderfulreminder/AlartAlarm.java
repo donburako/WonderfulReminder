6106b436b4e7813fd01af4c9d892ae04b5a641c6
YukariFujino <mokayumi55>
2016-07-14 17:39:54 +0900

    時間になった時に表示するダイアログをJavaクラスにて作成。 時間になったらこのファイルを呼び出すようにして、 OKボタン押した後の処理は作り直します

---
+++ app/src/main/java/com/reminder/jetpudding/wonderfulreminder/AlartAlarm.java
@@ -0,0 +1,31 @@
+package com.reminder.jetpudding.wonderfulreminder;
+
+/**
+ * Created by yukari1210 on 2016/07/14.
+ */
+
+import android.app.*;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class AlartAlarm extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_reminder);
+
+        AlertDialog.Builder dialogBuilder=new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("リマインダー");
+        dialogBuilder.setMessage("時間になりました");
+        dialogBuilder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog,int which){
+
+            }
+        });
+
+        dialogBuilder.setCancelable(true);
+        AlertDialog Dialog=dialogBuilder.create();
+        Dialog.show();
+    }
+}
